ad068cd4db857f108a5e49ed9dcd4c4d369fae9b
nkul72 <nkul72@users.noreply.github.com>
2018-12-05 17:00:54 -0600

    update repo

--- brigade.js
+++ brigade.js
@@ -58,9 +58,9 @@ function dockerJobRunner(config, d) {
 
 function kubeJobRunner (config, k) {
     k.storage.enabled = false
-    k.image = "kuberegbc/k8s-kubectl:v1.8.2"
+    k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
     k.tasks = [
-        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=<youracrhere>.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
+        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=kuberegbc.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
     ]
 }
 
